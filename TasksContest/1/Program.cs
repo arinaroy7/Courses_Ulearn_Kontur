a2ed6dfc3876072da2865d2bb0170a63314733ef
arinaroy7 <146703972+arinaroy7@users.noreply.github.com>
2025-03-10 17:26:15 +0400

    Made edits
    1 задание в контур 2024г.

---
+++ TasksContest/1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+class Program {
+    static void Main(string[] args) {
+
+        var n = Convert.ToInt32(Console.ReadLine());
+
+        var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        var index1 = 0;
+        var index2 = 0;
+        var max = int.MinValue;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue; // пропускаем
+                
+                var dif = numbers[i] - numbers[j];
+                if (dif > max) {
+                    max = dif; // если текущая разность больше, чем макс, то все обновляем
+                    index1 = i;
+                    index2 = j;
+                }
+                else if (dif == max && i - j > index1 - index2) {
+                        index1 = i;
+                        index2 = j;
+                }
+            }
+        }
+        Console.WriteLine((index1 + 1) + " " + (index2+1));    
+    }
+}
